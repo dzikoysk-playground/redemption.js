f19616791b68e89d81b83e80f2c70663539e15aa
dzikoysk <dzikoysk@dzikoysk.net>
2016-12-17 21:05:05 +0100

    Added Component.

---
+++ src/content/Component.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function Component(element) {
+    this.element = element;
+    this.childs = {};
+}
+
+Component.prototype.addChild = function (component) {
+    this.childs.push(component);
+};
+
+Component.prototype.getElement = function () {
+    return this.element;
+};
+
+module.exports = Component;
