c4399af485bdb516274f42538a9583a56d18f0a0
dzikoysk <dzikoysk@dzikoysk.net>
2016-12-18 18:11:16 +0100

    Updated test sources.

--- test/script.js
+++ test/script.js
@@ -1,11 +1,14 @@
 var Redemption = require('../src/redemption.js');
-var redemptionFactory = new Redemption.RedemptionFactory();
 
-redemptionFactory.invoke(function () {
-    var template = new Redemption.RedemptionTemplate()
-        .declaration('{head}')
-        .declaration('{content}');
-});
+var redemptionFactory = new Redemption.RedemptionFactory();
+var template = new Redemption.RedemptionTemplate()
+    .declaration('{head}')
+    .declaration('{content}');
+
+var rom = redemptionFactory.create(template);
+
+var parent = document.getElementsByName('html');
+rom.amen(parent);
 
 function require(s) {
     return Redemption;
