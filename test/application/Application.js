5867b8ffde358b6c67e0e104a8f4e0e89fc52479
dzikoysk <dzikoysk@dzikoysk.net>
2017-11-29 20:01:57 +0100

    Improve performance

--- test/application/Application.js
+++ test/application/Application.js
@@ -17,17 +17,10 @@
 var redemption = new Redemption();
 redemption.enableModifications();
 
-redemption.loadDependencies('./application/components/', [
-    'HeaderComponent.js',
-    'ContentComponent.js'
-]);
-
-redemption.onload(function () {
-    redemption.initializeStructure({
-        head: HeaderComponent,
-        content: ContentComponent
-    });
-
-    redemption.disableModifications();
-    redemption.render();
+redemption.initializeStructure({
+    head: HeaderComponent,
+    content: ContentComponent
 });
+
+redemption.disableModifications();
+redemption.render();
